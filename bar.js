60b22400d37bed30da48c9c1da2fbbfa93950f6d
BigDaddy-Zephyr <anujduggal61@gmail.com>
2019-08-29 19:36:54 +0530

    Created an array and made selction random.console.log("LOL"); faker=require("faker"); //console.log(faker.name.firstName()); //console.log(faker.name.lastName()); //let booksl=["a","b","c","d","e","f","anuj","afzal","pranavi"]; //console.log(booksl[faker.random.number({min:0,max:8})]); console.log(faker.random.arrayElement(["anuj","afzal","pranavi"]));

---
+++ bar.js
@@ -0,0 +1,8 @@
+console.log("LOL");
+faker=require("faker");
+//console.log(faker.name.firstName());
+//console.log(faker.name.lastName());
+//let booksl=["a","b","c","d","e","f","anuj","afzal","pranavi"];
+//console.log(booksl[faker.random.number({min:0,max:8})]);
+console.log(faker.random.arrayElement(["anuj","afzal","pranavi"]));
+
